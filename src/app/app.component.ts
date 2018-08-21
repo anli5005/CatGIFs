733dec00ae68133d99ec2d7f89083ebd68f35ce5
Anthony Li <li.anthony917@gmail.com>
2018-08-21 13:50:25 -0400

    GIF-ify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+export interface Image {
+  href?: string;
+  src: string;
+  alt?: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'CatGIFs';
+export class AppComponent implements OnInit {
+  times = 500;
+  images: Image[] = [];
+
+  ngOnInit() {
+    for (let i = 0; i < this.times; i++) {
+      this.images.push({
+        href: "https://en.wikipedia.org/wiki/Cat",
+        src: "https://media.giphy.com/media/WYEWpk4lRPDq0/giphy.gif",
+        alt: "MEOW"
+      });
+    }
+  }
 }
